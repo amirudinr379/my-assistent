df12306bec591b103868d96c1e72a0c001c2785a
amirudinr379 <amirudinr379@gmail.com>
2024-11-08 09:13:53 +0000

    update

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
-import Base64 from 'base64-js';
 import MarkdownIt from 'markdown-it';
 import { maybeShowApiKeyBanner } from './gemini-api-banner';
 import './style.css';
@@ -38,34 +37,6 @@ form.onsubmit = async (ev) => {
   output.textContent = 'Generating...';
 
   try {
-    // Load the image as a base64 string
-    // let imageUrl = form.elements.namedItem('chosen-image').value;
-    // let imageBase64 = await fetch(imageUrl)
-    //   .then(r => r.arrayBuffer())
-    //   .then(a => Base64.fromByteArray(new Uint8Array(a)));
-
-    // Assemble the prompt by combining the text with the chosen image
-    // let contents = [
-    //   {
-    //     role: 'user',
-    //     parts: [
-    //       // { inline_data: { mime_type: 'image/jpeg', data: imageBase64, } },
-    //       { text: promptInput.value }
-    //     ]
-    //   }
-    // ];
-
-    // Call the multimodal model, and get a stream of results
-    // const genAI = new GoogleGenerativeAI(API_KEY);
-    // const model = genAI.getGenerativeModel({
-    //   model: "gemini-pro", // or gemini-1.5-pro
-    //   safetySettings: [
-    //     {
-    //       category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-    //       threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-    //     },
-    //   ],
-    // });
 
     const prompt = promptInput.value;
     const result = await chat.sendMessageStream(prompt);
